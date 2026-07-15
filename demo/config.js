ee3c08a914ce3c0bf35b5dcd1de726926a5f3660
zhizi1005 <zhzi1005@163.com>
2026-07-15 18:28:27 +0800

    no message

--- demo/config.js
+++ demo/config.js
@@ -10,17 +10,18 @@ window.PaySdkDemoConfig = {
   },
 
   googlePay: {
-    merchantName: 'Demo Merchant',
+    merchantName: 'Alchemy Pay Ramp',
     /** Required when environment is PRODUCTION (example 8.4) */
-    merchantId: 'BCR2XXXXXXXXXXXXXX',
+    merchantId: 'BCR2DN4TQTA5V4YV', //BCR2DN5TRCG6H2QZ
     /** PAYMENT_GATEWAY mode */
     gateway: 'example',
     gatewayMerchantId: 'exampleGatewayMerchantId',
     /** Used by PRODUCTION example (8.4) */
-    productionGateway: 'stripe',
-    productionGatewayMerchantId: 'acct_123',
+    productionGateway: 'unlimint',
+    productionGatewayMerchantId: 'BCR2DN4TQTA5V4YV',
     /** DIRECT mode public key (replace with your real key) */
-    publicKey: 'BOdoXP+9Aq473S...'
+    publicKey:
+      'BE6v5sWsfYnUTgU+21rbWKcCAgPBuN8aR7k3b2tq+UMF6iuwHS1Px3maVxaRdbxUOn1HYuMWQ6Uvhc6/OhXE/p4='
   },
 
   applePay: {
